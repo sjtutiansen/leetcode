c18107a7f72a07cde041c8f379d9a3bfa280f6b8
tiansen <sjtutiansen@outlook.com>
2018-06-06 00:09:54 +0800

    add name

--- test/main.c
+++ test/main.c
@@ -1,5 +1,6 @@
 #include "leet_code.h"
 
+
 int main(int argc, char *argv[]){
     int nums[3] = {3,2,3};
     int numsSize = 3,target = 6;
--- test/the_sum_of_two_int.c
+++ test/the_sum_of_two_int.c
@@ -1,3 +1,10 @@
+/*
+Given an array of integers, return indices of the two numbers 
+such that they add up to a specific target.
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+*/
+
 #include "leet_code.h"
 
 int comp(const void*a,const void*b)//用来做比较的函数。  
